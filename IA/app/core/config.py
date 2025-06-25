57baee67f4a9f630404be34ea865755cdce38d8f
SCDaFlo <scastrof.94@gmail.com>
2025-06-24 20:16:01 -0500

    Basic Config

---
+++ IA/app/core/config.py
@@ -0,0 +1,21 @@
+
+"""✔️ Rol:
+Esta carpeta maneja la configuración central del proyecto, como:
+Variables de entorno
+Configuración de LangChain o Bedrock
+Conexiones a servicios (como AWS, Redis, S3)
+Logger global
+Middlewares generales (CORS, errores, etc.)"""
+
+
+import os
+from dotenv import load_dotenv
+
+if os.getenv("ENV", "local") == "local":
+    from dotenv import load_dotenv
+    load_dotenv()
+
+AWS_REGION = os.getenv("AWS_REGION", "us-east-1")
+BEDROCK_MODEL_ID = os.getenv("BEDROCK_MODEL_ID", "amazon.nova-micro-v1:0")
+DYNAMODB_TABLE = os.getenv("DYNAMODB_TABLE", "ChatMessages")
+LOCAL_PROFILE_NAME=os.getenv("LOCAL_PROFILE_NAME", "HousyProject")
