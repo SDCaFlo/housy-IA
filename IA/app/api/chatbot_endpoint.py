6d9f9349fefbfa99c60c77c7a144c30b07ed2131
SCDaFlo <scastrof.94@gmail.com>
2025-07-01 16:36:19 -0500

    Reestructuración del código de Eve

--- IA/app/api/chatbot_endpoint.py
+++ IA/app/api/chatbot_endpoint.py
@@ -3,7 +3,6 @@ from app.models.ChatMessage import UserMessage, ChatResponse
 from app.services.chatbot_engine import proccess_chat_turn
 from app.utils.intention_detection import tiene_intencion_busqueda
 from app.services.embeddings.search_opensearch import search_similar_properties
-from app.services.embeddings.bedrock_service import embed_text
 
 router = APIRouter()
 
@@ -15,21 +14,21 @@ async def chat_endpoint(payload: UserMessage):
         conv_id = payload.conv_id
 
         # 1️⃣ Intención de búsqueda
-        if tiene_intencion_busqueda(message):
-            resultados = search_similar_properties(message, k=3)
+        # if tiene_intencion_busqueda(message):
+        #     resultados = search_similar_properties(message, k=3)
 
-            if resultados:
-                resultados_ordenados = sorted(resultados, key=lambda x: x['score'], reverse=True)
+        #     if resultados:
+        #         resultados_ordenados = sorted(resultados, key=lambda x: x['score'], reverse=True)
 
-                texto_recomendacion = "🏡 Estas propiedades podrían interesarte:\n\n"
-                for i, r in enumerate(resultados_ordenados, 1):
-                    texto_recomendacion += (
-                        f"🏠 Propiedad recomendada #{i} (Score: {r['score']:.4f}):\n"
-                        f"{r['text']}\n\n"
-                    )
-                return ChatResponse(output=texto_recomendacion)
-            else:
-                return ChatResponse(output="No encontramos propiedades que coincidan. ¿Querés intentar con otra búsqueda?")
+        #         texto_recomendacion = "🏡 Estas propiedades podrían interesarte:\n\n"
+        #         for i, r in enumerate(resultados_ordenados, 1):
+        #             texto_recomendacion += (
+        #                 f"🏠 Propiedad recomendada #{i} (Score: {r['score']:.4f}):\n"
+        #                 f"{r['text']}\n\n"
+        #             )
+        #         return ChatResponse(output=texto_recomendacion)
+        #     else:
+        #         return ChatResponse(output="No encontramos propiedades que coincidan. ¿Querés intentar con otra búsqueda?")
 
         # 2️⃣ Flujo normal del chatbot
         response = proccess_chat_turn(
