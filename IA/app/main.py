a9c833e56f8ae80184ae61d47f274eb8ea99f27a
SCDaFlo <scastrof.94@gmail.com>
2025-07-02 15:19:45 -0500

    Added CORS implementation for FRONTEND

--- IA/app/main.py
+++ IA/app/main.py
@@ -2,10 +2,28 @@ from fastapi import FastAPI
 from app.api.chatbot_endpoint import router
 from app.api.chatbot_recovery import router as chat_router
 from app.api.embed_endpoint import router as embed_router
+from fastapi.middleware.cors import CORSMiddleware
+
+
+
 
 app = FastAPI(
     title="Chatbot API",
     version = "1.0.0",
+)
+
+
+## Configuración de CORS para seguridad
+origins = [
+    "*",  # Esto permite todo (no recomendado en producción)
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Lista de orígenes permitidos
+    allow_credentials=True,
+    allow_methods=["*"],  # También puedes poner solo ["GET", "POST", ...]
+    allow_headers=["*"],
 )
 
 # Incluye la ruta de los archivos  api/routes.py
